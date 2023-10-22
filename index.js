3de5c4f0ae0358d1ea0d6d29791f8100739d2bc3
Isinwa-Deborah <debbyisinwa@gmail.com>
2023-10-22 16:38:51 -0700

    modified codes

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const mongoose = require("mongoose");
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfghjklyuiop';
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(cookieParser())
 
-mongoose.connect('mongodb+srv://bloguser:jZWpuabIo2y1wFot@cluster0.g8rcclh.mongodb.net/?retryWrites=true&w=majority');
+dotenv.config({ path: './config.env' })
+
+mongoose.connect(process.env.MONGODB_URL);
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
@@ -40,12 +45,27 @@ app.post('/login', async (req, res) => {
         //logged in
         jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token).json('ok');
+            res.cookie('token', token).json({
+                id: UserDoc._id,
+                username,
+            });
         })
 
     } else {
         res.status(400).json('wrong credentials');
     }
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
 })
 
 app.listen(4000);
