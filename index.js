8cbfbf0eb8ede4d2a97419c60ab6d983bf5d33c8
Isinwa-Deborah <debbyisinwa@gmail.com>
2023-10-23 01:31:39 -0700

    Added functions

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const cors = require('cors');
 const app = express();
 const mongoose = require("mongoose");
 const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' })
+const fs = require('fs');
+
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfghjklyuiop';
@@ -39,8 +44,7 @@ app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const UserDoc = await User.findOne({ username });
     const passOk = bcrypt.compareSync(password, UserDoc.password);
-    // res.json(passOk);
-    // res.json(UserDoc);
+
     if (passOk) {
         //logged in
         jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
@@ -66,6 +70,35 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
-})
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
+        })
+        res.json({ postDoc });
+        // res.json(info);
+    });
+
+
+});
+
+app.get('/post', async (req, res) => {
+    res.json(await Post.find().populate('author', ['username']));
+});
 
 app.listen(4000);
